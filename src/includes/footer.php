0d71e3f5068fb05e68313a492a444e1e83060432
Felbrou <felbrou@gmail.com>
2025-08-18 15:42:52 -0300

    conding header and footer of the project

---
+++ src/includes/footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer class="site-footer">
+        <div class="container">
+            <p>&copy; 2025 Verissimo E-commerce Store. All rights reserved.</p>
+        </div>
+    </footer>
+    <script src="/js/main.js"></script>
+</body>
+</html>
