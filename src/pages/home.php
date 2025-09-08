367c06ff27a32d942471cd5b328d2546f0ca2ad1
Felbrou <felbrou@gmail.com>
2025-09-08 15:53:12 -0300

    adding styles on homepage

--- src/pages/home.php
+++ src/pages/home.php
@@ -14,70 +14,70 @@ require __DIR__ . '/../includes/header.php';
 <section class="featured products">
     <div class="container">
         <h2>Featured Products</h2>
-        <div class="product-grid">
             <!-- Example product item -->
              <div class="vv-padding vv-center vv-row-padding vv-display-block">
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product1.jpg';?> alt="product1" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product1.jpg" alt="product1" style="width:100%; height:400px;">
                     <h3>
                         Product 1
                     </h3>
                     <p>
                         Description for product 1.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
             </div>
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product2.jpg';?> alt="product2" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product2.jpg" alt="product2" style="width:100%; height:400px;">
                     <h3>
                         Product 2
                     </h3>
                     <p>
                         Description for product 2.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
                 </div>
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product3.jpg';?> alt="product3" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product3.jpg" alt="product3" style="width:100%; height:400px;">
                     <h3>
                         Product 3
                     </h3>
                     <p>
                         Description for product 3.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
                 </div>
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product4.jpg';?> alt="product4" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product4.jpg" alt="product4" style="width:100%; height:400px;">
                     <h3>
                         Product 4
                     </h3>
                     <p>
                         Description for product 4.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
                 </div>
-                    </h3>
-                    <p></p>
-                </div>
-
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product5.jpg';?> alt="product5" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product5.jpg" alt="product5" style="width:100%; height:400px;">
                     <h3>
                         Product 5
                     </h3>
                     <p>
                         Description for product 5.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
                 </div>
 
-                <div class="item">
-                    <img src=<?php __DIR__ . '/../public/css/images/product6.jpg';?> alt="product6" style="width:100%">
+                <div class="product-card">
+                    <img src="css/images/product6.jpg" alt="product6" style="width:100%; height:400px;">
                     <h3>
                         Product 6
                     </h3>
                     <p>
                         Description for product 6.
                     </p>
+                    <a href="" class="btn btn-primary">Add to Cart</a>
                 </div>
              </div>
-        </div>
     </div>
 </section>
 
